4fe94fdb778c8be03d705140941981de36f2f07f
An Bui <ducan2408@gmail.com>
2019-04-20 20:30:22 +0700

    Init structure for each answer.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'question1/answer', to: 'home#question1'
+  get 'question2/answer', to: 'home#question2'
+  get 'question3/answer', to: 'home#question3'
+  get 'question4/answer', to: 'home#question4'
+  get 'question5/answer', to: 'home#question5'
+  get 'question6/answer', to: 'home#question6'
+
+  root to: 'home#question1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
